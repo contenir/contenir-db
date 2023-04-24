2876689b321856a0fc99c267101059c9475f255b
Simon Mundy <simon.mundy@peptolab.com>
2023-04-24 05:16:54 +0000

    Improvements to BaseResourceEntity

--- src/Entity/ResourceEntity.php
+++ src/Entity/ResourceEntity.php
@@ -2,9 +2,18 @@
 
 namespace Contenir\Db\Model\Entity;
 
-abstract class ResourceEntity extends AbstractEntity
+use Contenir\Metadata\MetadataInterface;
+
+abstract class BaseResourceEntity extends AbstractEntity implements
+    MetadataInterface
 {
     protected $routeId;
+    protected $routePath;
+
+    public function getResourceId()
+    {
+        return 'resource';
+    }
 
     /**
      * getRouteId
@@ -26,5 +35,50 @@ abstract class ResourceEntity extends AbstractEntity
         }
 
         return sprintf('%s', join('/', array_filter([$this->routeId, $path])));
+    }
+
+    /**
+     * getRoutePath
+     *
+     * @return String
+     */
+    public function getRoutePath(): string
+    {
+        if ($this->routePath === null) {
+            $parts           = explode('/', (string) $this->slug);
+            $this->routePath = sprintf('/%s', join('/', array_filter($parts)));
+        }
+
+        return $this->routePath;
+    }
+
+    public function getMetaTitle()
+    {
+        $fallbackTitle = join(' ', array_filter([
+            $this->title ?? null,
+            $this->subtitle ?? null
+        ]));
+
+        return $this->meta_title ?? $fallbackTitle;
+    }
+
+    public function getMetaDescription()
+    {
+        return $this->meta_description ?? $this->description ?? null;
+    }
+
+    public function getMetaImage()
+    {
+        return $this->image[0]->path ?? null;
+    }
+
+    public function getMetaModified()
+    {
+        return $this->updated ?? null;
+    }
+
+    public function getMetaPublish()
+    {
+        return $this->created ?? null;
     }
 }
